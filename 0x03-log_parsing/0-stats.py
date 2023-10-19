b4c5b1edef824583f36f7b3c337172c312a6fe85
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-19 01:46:10 -0700

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,54 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import sys
-import re
 
 
-# Initialize variables to store metrics
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
 total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        # Use regex to parse the line
-        match = re.match(r'(\d+\.\d+\.\d+\.\d+) - \[.+\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)', line)
-        if match:
-            ip, status_code, file_size = match.groups()
-            status_code = int(status_code)
-            file_size = int(file_size)
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-            # Update metrics
-            total_file_size += file_size
-            status_code_counts[status_code] += 1
-            line_count += 1
+        if len(parsed_line) > 2:
+            counter += 1
 
-        # Print metrics every 10 lines
-        if line_count % 10 == 0:
-            print("Total file size: File size: {}".format(total_file_size))
-            for code, count in sorted(status_code_counts.items()):
-                if count > 0:
-                    print("{}: {}".format(code, count))
-            print()
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-except KeyboardInterrupt:
-    pass
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-# Print final metrics
-print("Total file size: File size: {}".format(total_file_size))
-for code, count in sorted(status_code_counts.items()):
-    if count > 0:
-        print("{}: {}".format(code, count))
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
